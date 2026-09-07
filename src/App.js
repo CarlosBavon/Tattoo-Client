1f9b32609d49f4035cd650ce7313ecbd4ed931f1
CarlosBavon <carlosbavon46@gmail.com>
2026-09-08 01:41:12 +0300

    Adding Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/artists" element={<div>Artists</div>} />
+        <Route path="/gallery" element={<div>Gallery</div>} />
+        <Route path="/services" element={<div>Services</div>} />
+        <Route path="/aftercare" element={<div>Aftercare</div>} />
+        <Route path="/about" element={<div>About</div>} />
+        <Route path="/contact" element={<div>Contact</div>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "../assets/images/logo.png";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
+function Navbar () {
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <img src={logo} alt="Logo" />
+            </div>
+            <ul className="nav-links">
+                <li><Link to="/artists">Artists</Link></li>
+                <li><Link to="/gallery">Gallery</Link></li>
+                <li><Link to="/services">Services</Link></li>
+                <li><Link to="/aftercare">Aftercare</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+            </ul>
+            <Link to="/booking" className="book-now-button">Book Appointment</Link>
+        </nav>
+    );
+}
+
+export default Navbar;
